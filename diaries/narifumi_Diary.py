1571eff8620911eaec5ffb0d7db14212b16bf643
narifumi0508 <furapote03@gmail.com>
2024-11-28 15:51:27 +0900

    narifumi_Diary

---
+++ diaries/narifumi_Diary.py
@@ -0,0 +1,12 @@
+from diaries.AbstractDiary import AbstractDiary
+
+class narifumi_Diary(AbstractDiary):
+
+    def get_date(self):
+        return "2024-11-28"
+
+    def get_summary(self):
+        return "大変な日だった"
+
+    def get_author(self):
+        return "narifumi"
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from diaries.DiarySample import DiarySample
+from diaries.narifumi_Diary import narifumi_Diary
 
 # ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), ] 
+diaries = [DiarySample(),
+           narifumi_Diary() ] 
 
 for d in diaries:
     print("---------------------------------")

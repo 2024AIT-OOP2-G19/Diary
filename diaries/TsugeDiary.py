9208b5273d234648189c09b3693b805a0c15abcd
k22232 <k22232kk@aitech.ac.jp>
2024-11-28 15:40:03 +0900

    adddiary
    Update

---
+++ diaries/TsugeDiary.py
@@ -0,0 +1,12 @@
+from diaries.AbstractDiary import AbstractDiary
+
+class TsugeDiary(AbstractDiary):
+
+    def get_date(self):
+        return "2024-11-28"
+
+    def get_summary(self):
+        return "なにもない一日だった"
+
+    def get_author(self):
+        return "Tsuge"
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from diaries.DiarySample import DiarySample
+from diaries.TsugeDiary import TsugeDiary
 
 # ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), ] 
+diaries = [DiarySample(), 
+           TsugeDiary()
+           ] 
 
 for d in diaries:
     print("---------------------------------")
